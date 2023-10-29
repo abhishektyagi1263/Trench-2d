c765ee1f703ba401a0aae4fb9d16709640646f60
Your Name <you@example.com>
2023-10-29 17:37:01 +0530

    third c

--- js/main.js
+++ js/main.js
@@ -8,10 +8,17 @@ audio.play();
 let gameEnded=false;
 let rows = [];
 let player1 = document.getElementById('player1');
-let player1CurrentCood = {
-    top: 1, left: 1
-}
+// let player2 = document.getElementById('player2');
 const size = 8;
+let player1Info = {
+    top: 1, left: 1, name: ''
+};
+let player2Info = {
+    top: size-1, left: size-1, name: ''
+};
+let isPlayer1Turn = true;
+
+
 let mineFeild = [];
 function mineGenerator() {
     let x = Math.random() * 10;
@@ -35,6 +42,10 @@ function generateField(size) {
 
 
 function createBoard(size) {
+    p1=prompt("Enter Player 1 Name");
+    player1Info
+    p2=prompt("Enter Player 2 Name");
+
     generateField(size);
     for (let rowNum = 0; rowNum < size; rowNum++) {
         let row = document.createElement("div");
@@ -44,53 +55,100 @@ function createBoard(size) {
             tile.classList.add('tile');
             tile.setAttribute('id', rowNum + '-' + colNum);
             tile.addEventListener('click', movePlayer);
-
+            if (rowNum==0 &&colNum==0) {
+                tile.classList.add("p1-camp")
+            }
             row.appendChild(tile);
         }
         rows.push(row);
         playground[0].appendChild(row);
 
     }
-
+    document.getElementById('p1').innerText=p1;
+    document.getElementById('p2').innerText=p2;
+    player1Info.name=p1;
+    player2Info.name=p2;
+    setUpPlayers() ;
+    whoseTurn();
     console.log(rows);
 }
 
 createBoard(size);
+
+function whoseTurn() {
+    if (isPlayer1Turn) {
+        document.getElementById('turn').innerText='Player - '+player1Info.name.toUpperCase()+' Turn'
+    }
+    else{document.getElementById('turn').innerText='Player - '+player2Info.name.toUpperCase()+' Turn'}
+}
+function setUpPlayers() {
+    player1.style.top = (0) + "rem";
+    player1.style.left = (0) + "rem";
+
+    player2.style.top = (player2Info.top * 5.4) + "rem";
+    player2.style.left = (player2Info.left * 5.4) + "rem";
+}
+
 function movePlayer(event) {
     if (gameEnded) {
-        alert("game has eneded");
+        alert("game has ended".toUpperCase());
         restartGame();
     }
     console.log(event);
 
 
     tileId = event.target.id;
+    let oldPlayerCoods=null;
+    let player = null;
+    if (isPlayer1Turn) {
+         oldPlayerCoods=player1Info; 
+        player = player1;
+    }
+    else{
+         oldPlayerCoods=player2Info; 
+         player=player2;
+    }
+    
 
     playerNewCoodinates = tileId.split("-");
     let move = {
         top: Number(playerNewCoodinates[1]), left: Number(playerNewCoodinates[0])
     }
 
-    if (isValidMove(move)) {
+    if (isValidMove(move,oldPlayerCoods)) {
         console.table(Number(playerCurrentLeft), Number(playerNewCoodinates[0])
         );
+        if (isPlayer1Turn) {
+        player1.style.top = (playerNewCoodinates[1] * 5.4) + "rem";
+        player1.style.left = (playerNewCoodinates[0] * 5.4) + "rem";
+        player1Info.left = playerNewCoodinates[0];
+        player1Info.top = playerNewCoodinates[1];
+        
+    }
 
-        player1.style.top = (playerNewCoodinates[1] * 6.1) + "rem";
-        player1.style.left = (playerNewCoodinates[0] * 6.1) + "rem";
-        player1CurrentCood.left = playerNewCoodinates[0];
-        player1CurrentCood.top = playerNewCoodinates[1];
-        if (won(move)) { 
+    if (!isPlayer1Turn) {
+        player2.style.top = (playerNewCoodinates[1] * 5.4) + "rem";
+        player2.style.left = (playerNewCoodinates[0] * 5.4) + "rem";
+        player2Info.left = playerNewCoodinates[0];
+        player2Info.top = playerNewCoodinates[1];
+        
+    }
+        if (won(move,isPlayer1Turn)) { 
             
-            setTimeout(()=>{alert('YOU WON');restartGame()}, 500)
+            setTimeout(()=>{alert(oldPlayerCoods.name+' YOU WON');restartGame()}, 500)
         
         }
         if (check4Mines(move)) {
             
             explodedTile=document.getElementById(tileId);
             explodedTile.classList.add('mine');
-            setTimeout(()=>{alert('YOU LOSE');restartGame()}, 500)
+            setTimeout(()=>{alert(oldPlayerCoods.name+' YOU LOSE');restartGame()}, 500)
             
             
+        }
+        if (!check4Mines(move)&&!won(move)) {
+            isPlayer1Turn=!isPlayer1Turn;
+            whoseTurn();
         }
 
     }
@@ -102,9 +160,9 @@ function movePlayer(event) {
 
 }
 
-function isValidMove(move) {
-    playerCurrentTop = player1CurrentCood.top;
-    playerCurrentLeft = player1CurrentCood.left;
+function isValidMove(move,oldPlayerCoods) {
+    playerCurrentTop = oldPlayerCoods.top;
+    playerCurrentLeft = oldPlayerCoods.left;
     return (
         (Number(playerCurrentLeft) + 1 == Number(move.left)
             || Number(playerCurrentLeft) - 1 == Number(move.left)
@@ -116,8 +174,8 @@ function isValidMove(move) {
 
 }
 
-function won() {
-    return player1CurrentCood.left == size - 1 && player1CurrentCood.right==size-1;
+function won(move,turn) {
+    return (player1Info.left == size - 1 && player1Info.top==size-1) && turn || (player2Info.left == 1-1 && player2Info.top==1-1) && !turn;
 }
 
 function check4Mines(move) {
